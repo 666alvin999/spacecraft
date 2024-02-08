1ae58ed3646e365ba9a761c4e1d8952320b91d7f
Alvin Hamaïde <alvin98mc@gmail.com>
2024-02-08 12:59:42 +0100

    problems

--- App.tsx
+++ App.tsx
@@ -5,25 +5,25 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { StarshipFeedScreen } from "./src/screen";
 
-function App() {
-    const queryClient = new QueryClient();
-    return (
-        <>
-            <NetworkProvider>
-                <QueryClientProvider client={queryClient}>
-                    {/*<LoginScreen/>*/}
-                    <PaperProvider>
-                        <SafeAreaView />
-                        {/*<ScrollView>*/}
-                        {/*    <TermsScreen/>*/}
-                        {/*</ScrollView>*/}
-                        <StarshipFeedScreen />
-                    </PaperProvider>
-                </QueryClientProvider>
-            </NetworkProvider>
-        </>
-    );
-}
+// function App() {
+//     const queryClient = new QueryClient();
+//     return (
+//         <>
+//             <NetworkProvider>
+//                 <QueryClientProvider client={queryClient}>
+//                     {/*<LoginScreen/>*/}
+//                     <PaperProvider>
+//                         <SafeAreaView />
+//                         {/*<ScrollView>*/}
+//                         {/*    <TermsScreen/>*/}
+//                         {/*</ScrollView>*/}
+//                         <StarshipFeedScreen />
+//                     </PaperProvider>
+//                 </QueryClientProvider>
+//             </NetworkProvider>
+//         </>
+//     );
+// }
 
 // export default App;
 
