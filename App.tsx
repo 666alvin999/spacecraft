378e15c7f9b67baa9d9a662b118698089749b543
Alvin Hamaïde <alvin98mc@gmail.com>
2024-02-08 12:11:31 +0100

    Storybook

--- App.tsx
+++ App.tsx
@@ -1,26 +1,30 @@
-import {StarshipFeedScreen} from "./src/screen";
-import {PaperProvider} from "react-native-paper";
-import {SafeAreaView} from "react-native";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {NetworkProvider} from 'react-native-offline';
+import { SafeAreaView } from "react-native";
+import { NetworkProvider } from "react-native-offline";
+import { PaperProvider } from "react-native-paper";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export default function App() {
-    const queryClient = new QueryClient
+import { StarshipFeedScreen } from "./src/screen";
+
+function App() {
+    const queryClient = new QueryClient();
     return (
         <>
             <NetworkProvider>
                 <QueryClientProvider client={queryClient}>
                     {/*<LoginScreen/>*/}
                     <PaperProvider>
-                        <SafeAreaView/>
+                        <SafeAreaView />
                         {/*<ScrollView>*/}
                         {/*    <TermsScreen/>*/}
                         {/*</ScrollView>*/}
-                        <StarshipFeedScreen/>
+                        <StarshipFeedScreen />
                     </PaperProvider>
                 </QueryClientProvider>
             </NetworkProvider>
         </>
-
     );
 }
+
+// export default App;
+
+export { default } from "./.storybook";
